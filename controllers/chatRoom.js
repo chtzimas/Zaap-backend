b88515807c044e2891c015084e0873610a0bdd7c
Christos Tzimas <tzimas13@hotmail.com>
2022-07-08 16:44:15 +0300

    add ChatRoom model/update node

--- controllers/chatRoom.js
+++ controllers/chatRoom.js
@@ -1,5 +1,29 @@
+import makeValidation from '@withvoid/make-validation';
+import ChatRoomModel, {CHAT_ROOM_TYPES} from "../models/ChatRoom.js";
+
 export default {
     initiate: async (req, res) => {
+        try {
+            const validation = makeValidation(types => ({
+                payload: req.body,
+                checks: {
+                    userIds: {
+                        type: types.array,
+                        options: { unique: true, empty: false, stringOnly: true }
+                    },
+                    type: { type: types.enum, options: { enum: CHAT_ROOM_TYPES } },
+                }
+            }));
+            if (!validation.success) return res.status(400).json({ ...validation });
+
+            const { userIds, type } = req.body;
+            const { userId: chatInitiator } = req;
+            const allUserIds = [...userIds, chatInitiator];
+            const chatRoom = await ChatRoomModel.initiateChat(allUserIds, type, chatInitiator);
+            return res.status(200).json({ success: true, chatRoom });
+        } catch (error) {
+            return res.status(500).json({ success: false, error: error.message })
+        }
     },
     postMessage: async (req, res) => {
     },
